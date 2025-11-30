b6cd44b985ed3a3761aeb614abce63b77fda3ce2
TobiasNetzer <netzerto@gmail.com>
2025-11-30 17:23:33 +0100

    Refactor AboutWindowViewModel.cs

--- ViewModels/AboutWindowViewModel.cs
+++ ViewModels/AboutWindowViewModel.cs
@@ -5,37 +5,23 @@ using CommunityToolkit.Mvvm.Input;
 
 namespace ATLab.ViewModels;
 
-public partial class AboutWindowViewModel : ViewModelBase
+public partial class AboutWindowViewModel(IHardwareInfoProvider hardwareInfo) : ViewModelBase
 {
-    public string FirmwareVersion { get; }
-    public string DeviceName { get; }
-    public string BuildDate { get; }
-    public string BuildTime { get; set; }
+    public string FirmwareVersion { get; } = hardwareInfo.FirmwareVersion;
+    public string DeviceName { get; } = hardwareInfo.DeviceName;
+    public string BuildDate { get; } = hardwareInfo.BuildDate;
+    public string BuildTime { get; set; } = hardwareInfo.BuildTime;
 
-    public string MeasChannelCount { get; }
-    public string StimChannelCount { get; }
-    public string ExtStimChannelCount { get; }
+    public string MeasChannelCount { get; } = hardwareInfo.MeasChannelCount.ToString();
+    public string StimChannelCount { get; } = hardwareInfo.StimChannelCount.ToString();
+    public string ExtStimChannelCount { get; } = hardwareInfo.ExtStimChannelCount.ToString();
 
-    public string AppVersion => Assembly.GetEntryAssembly()!.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "Unknown";
-    public string Author => Assembly.GetEntryAssembly()!.GetCustomAttribute<AssemblyCompanyAttribute>()?.Company ?? "Unknown";
+    public static string AppVersion => Assembly.GetEntryAssembly()!.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "Unknown";
+    public static string Author => Assembly.GetEntryAssembly()!.GetCustomAttribute<AssemblyCompanyAttribute>()?.Company ?? "Unknown";
 
     public event Action? RequestClose;
 
-    public AboutWindowViewModel(IHardwareInfoProvider hardware)
-    {
-        FirmwareVersion = hardware.FirmwareVersion;
-        DeviceName  = hardware.DeviceName;
-        BuildDate = hardware.BuildDate;
-        BuildTime = hardware.BuildTime;
-        MeasChannelCount = hardware.MeasChannelCount.ToString();
-        StimChannelCount = hardware.StimChannelCount.ToString();
-        ExtStimChannelCount = hardware.ExtStimChannelCount.ToString();
-    }
-
     [RelayCommand]
-    private void Close()
-    {
-        RequestClose?.Invoke();
-    }
+    private void Close() => RequestClose?.Invoke();
 
 }
