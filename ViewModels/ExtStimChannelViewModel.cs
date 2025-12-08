ca9af631b46f008e1fe0c5ab8da7bd434792de80
TobiasNetzer <netzerto@gmail.com>
2025-12-08 19:57:44 +0100

    Add Dropdown to ExtStimSelector UserControl

--- ViewModels/ExtStimChannelViewModel.cs
+++ ViewModels/ExtStimChannelViewModel.cs
@@ -1,16 +1,22 @@
 using System.Collections.ObjectModel;
 using ATLab.Interfaces;
 using ATLab.Wrappers;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace ATLab.ViewModels;
 
-public class ExtStimChannelViewModel
+public partial class ExtStimChannelViewModel : ViewModelBase
 {
-    public ObservableCollection<RelayChannelViewModel> ExtStimChannels { get; } = new();
+    public ObservableCollection<RelayChannelViewModel> ExtStimChannels { get; }
+    
+    [ObservableProperty]
+    private bool _isExpanded;
 
     public ExtStimChannelViewModel(ITestHardware testHardware)
     {
         var extStimGroup = new ExtStimChannelGroup(testHardware);
+
+        ExtStimChannels = new ObservableCollection<RelayChannelViewModel>();
 
         for (int i = 0; i < testHardware.HardwareInfo.ExtStimChannelCount; i++)
         {
