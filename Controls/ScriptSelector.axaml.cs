8c527902fbe7bda2e15ec86f236e43787fb52ba2
TobiasNetzer <netzerto@gmail.com>
2025-12-28 00:26:00 +0100

    Add `ScriptSelector` and `SerialDeviceManager` controls with basic initialization logic

---
+++ Controls/ScriptSelector.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ATLab.Controls;
+
+public partial class ScriptSelector : UserControl
+{
+    public ScriptSelector()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
---
+++ Controls/SerialDeviceManager.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ATLab.Controls;
+
+public partial class SerialDeviceManager : UserControl
+{
+    public SerialDeviceManager()
+    {
+        InitializeComponent();
+    }
+}
