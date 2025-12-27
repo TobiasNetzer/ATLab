b639f2e9476f34b5f91dedfa21a0f8b0c8afcc6e
TobiasNetzer <netzerto@gmail.com>
2025-12-28 00:12:15 +0100

    Add `IScriptRunner` and `IScpiScriptService` interfaces to define script execution and management contracts.

---
+++ Interfaces/IScpiScriptService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using ATLab.ViewModels;
+
+namespace ATLab.Interfaces;
+
+public interface IScpiScriptService
+{
+    ObservableCollection<ScpiScriptItemViewModel> Scripts { get; }
+    Task LoadAllAsync();
+    Task SaveAsync(ScpiScriptItemViewModel script);
+    Task DeleteAsync(ScpiScriptItemViewModel script);
+    ScpiScriptItemViewModel CreateNew();
+}
---
+++ Interfaces/IScriptRunner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ATLab.Models;
+
+namespace ATLab.Interfaces;
+
+public interface IScriptRunner
+{
+    Task ExecuteAsync(string scriptId, string deviceName, IEnumerable<ScpiVariable> variables, CancellationToken token);
+    Task<T?> ExecuteAsync<T>(string scriptId, string deviceName, IEnumerable<ScpiVariable> variables, CancellationToken token);
+}
