ed767f062b16ad8e99d47916ede8ccef5fd12026
TobiasNetzer <netzerto@gmail.com>
2025-12-29 18:46:11 +0100

    Add new converters for enum comparison and test status visualization
    - Introduced `EnumEqualsToBoolConverter` and `InvertedEnumEqualsToBoolConverter` for enum-based value bindings.
    - Added `TestStatusToBrushConverter` to map test statuses to color brushes for enhanced UI feedback.
    - Updated `App.axaml` resources to include the new converters.

---
+++ Converters/TestStatusToBrushConverter.cs
@@ -0,0 +1,30 @@
+﻿using ATLab.Enums;
+
+namespace ATLab.Converters;
+
+using Avalonia.Media;
+using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+public class TestStatusToBrushConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not TestStatus status)
+            return Brushes.Gray;
+
+        return status switch
+        {
+            TestStatus.IDLE      => Brushes.Gray,
+            TestStatus.RUNNING   => Brushes.DodgerBlue,
+            TestStatus.PASSED    => Brushes.LimeGreen,
+            TestStatus.FAILED    => Brushes.Red,
+            TestStatus.CANCELLED => Brushes.Orange,
+            _ => Brushes.Gray
+        };
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
