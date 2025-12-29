75c588ed07d3532ce1c8d5b8c6d826a4ae8eacda
TobiasNetzer <netzerto@gmail.com>
2025-12-30 00:01:43 +0100

    Update `ResultBrushConverter` to use `LimeGreen` for improved color consistency

--- Converters/ResultBrushConverter.cs
+++ Converters/ResultBrushConverter.cs
@@ -11,7 +11,7 @@ public class ResultBrushConverter : IValueConverter
     {
         if (value is bool isValid)
         {
-            return isValid ? Brushes.LightGreen : Brushes.Red;
+            return isValid ? Brushes.LimeGreen : Brushes.Red;
         }
 
         return Brushes.Gray;
