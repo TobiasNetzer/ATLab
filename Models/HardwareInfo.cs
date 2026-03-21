bfc688df27ab28eaa5c7be820ec73c45720ea5e3
TobiasNetzer <netzerto@gmail.com>
2026-03-21 22:19:01 +0100

    add separate HardwareInfo model

---
+++ Models/HardwareInfo.cs
@@ -0,0 +1,18 @@
+﻿using ATLab.Interfaces;
+
+namespace ATLab.Models;
+
+public class HardwareInfo : IHardwareInfo
+{
+    public string DeviceName { get; set; } = string.Empty;
+    public string SerialNumber { get; set; } = string.Empty;
+    public string FirmwareVersion { get; set; } = string.Empty;
+    public string BuildDate { get; set; } = string.Empty;
+    public string BuildTime { get; set; } = string.Empty;
+    public int MeasChannelCount { get; set; }
+    public int StimChannelCount { get; set; }
+    public int ExtStimChannelCount { get; set; }
+    public bool InterfaceAvailableI2C { get; set; }
+    public bool InterfaceAvailableUART { get; set; }
+    public bool InterfaceAvailableRS485 { get; set; }
+}
