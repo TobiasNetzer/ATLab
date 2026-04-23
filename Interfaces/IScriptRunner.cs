1b0bd1c405528ff8ee3fefc3ca7b5b058e3b9140
TobiasNetzer <netzerto@gmail.com>
2026-04-23 20:18:50 +0200

    Rename `deviceName` to `deviceId`

--- Interfaces/IScriptRunner.cs
+++ Interfaces/IScriptRunner.cs
@@ -8,6 +8,6 @@ namespace ATLab.Interfaces;
 
 public interface IScriptRunner
 {
-    Task<OperationResult> ExecuteAsync(string scriptId, string deviceName, IEnumerable<ScriptVariable> variables, CancellationToken token);
-    Task<OperationResult<T>> ExecuteAsync<T>(string scriptId, string deviceName, IEnumerable<ScriptVariable> variables, CancellationToken token, ResponseMask? mask = null);
+    Task<OperationResult> ExecuteAsync(string scriptId, string deviceId, IEnumerable<ScriptVariable> variables, CancellationToken token);
+    Task<OperationResult<T>> ExecuteAsync<T>(string scriptId, string deviceId, IEnumerable<ScriptVariable> variables, CancellationToken token, ResponseMask? mask = null);
 }
