e1530cdaabf2ec83a9cbd8064c9997a21b4d9323
TobiasNetzer <netzerto@gmail.com>
2026-07-22 18:49:19 +0200

    Add `TestInterfaceConfig` model and related enums for serial communication configuration

---
+++ Enums/SerialParity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace ATLab.Enums;
+
+public enum SerialParity
+{
+    [Description("None")]
+    NONE,
+    [Description("Odd")]
+    ODD,
+    [Description("Even")]
+    EVEN,
+}
