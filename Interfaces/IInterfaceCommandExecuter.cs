9c679ffe9c01d6816df8ee752ea19d9aec84d520
TobiasNetzer <netzerto@gmail.com>
2026-07-24 22:44:38 +0200

    add InterfaceCommandExecuter class

---
+++ Interfaces/IInterfaceCommandExecuter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ATLab.Models;
+
+namespace ATLab.Interfaces;
+
+public interface IInterfaceCommandExecuter
+{
+    Task<OperationResult<double>> ExecuteAsync(
+        TestInterfaceConfig config,
+        List<CustomVariable> runtimeVariables,
+        ResponseMask? mask);
+}
