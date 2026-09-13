939a78e560fad736cba84134de1b19f22e2b1db0
TobiasNetzer <netzerto@gmail.com>
2026-09-13 15:41:02 +0200

    Fix autoscrolling behavior

--- Behaviors/DataGridSelectedItemsBehavior.cs
+++ Behaviors/DataGridSelectedItemsBehavior.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.VisualTree;
+using Avalonia.Threading;
 
 namespace ATLab.Behaviors;
 
@@ -19,26 +21,61 @@ public static class DataGridSelectedItemsBehavior
 
     static DataGridSelectedItemsBehavior()
     {
-        SelectedItemsProperty.Changed.Subscribe(new Observer());
+        SelectedItemsProperty.Changed.Subscribe(new SelectedItemsObserver());
     }
 
-    private class Observer : IObserver<AvaloniaPropertyChangedEventArgs<IList?>>
+    private class SelectedItemsObserver : IObserver<AvaloniaPropertyChangedEventArgs<IList?>>
     {
         public void OnNext(AvaloniaPropertyChangedEventArgs<IList?> args)
         {
-            if (args.Sender is DataGrid grid)
+            if (args.Sender is not DataGrid grid)
+                return;
+
+            grid.AttachedToVisualTree -= GridOnAttached;
+            grid.AttachedToVisualTree += GridOnAttached;
+        }
+
+        private void GridOnAttached(object? sender, VisualTreeAttachmentEventArgs e)
+        {
+            if (sender is not DataGrid grid)
+                return;
+
+            grid.SelectionChanged -= GridOnSelectionChanged;
+            grid.SelectionChanged += GridOnSelectionChanged;
+        }
+
+        private static void GridOnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+        {
+            if (sender is not DataGrid grid)
+                return;
+
+            var target = GetSelectedItems(grid);
+            if (target == null)
+                return;
+
+            target.Clear();
+            foreach (var item in grid.SelectedItems)
+                target.Add(item);
+
+            if (grid.SelectedItem == null)
+                return;
+            
+            Dispatcher.UIThread.Post(() =>
             {
-                grid.SelectionChanged += (_, __) =>
-                {
-                    var target = GetSelectedItems(grid);
-                    if (target != null)
-                    {
-                        target.Clear();
-                        foreach (var item in grid.SelectedItems)
-                            target.Add(item);
-                    }
-                };
+                var row = FindRowByDataContext(grid, grid.SelectedItem);
+                row?.BringIntoView();
+            });
+        }
+
+        private static Control? FindRowByDataContext(DataGrid grid, object item)
+        {
+            foreach (var v in grid.GetVisualDescendants())
+            {
+                if (v is Control c && c.DataContext == item)
+                    return c;
             }
+
+            return null;
         }
 
         public void OnCompleted() { }
